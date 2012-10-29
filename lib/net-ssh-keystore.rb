cb8475667f268a5b8737cfe6fa401f45b25207d0
Ernie Brodeur <ebrodeur@ujami.net>
2012-10-28 19:05:37 -0700

    Moved a few files around to support a better loading scheme.

--- lib/net-ssh-keystore.rb
+++
@@ -1,9 +0,0 @@
-require "net-ssh-keystore/version"
-
-module Net
-  module Ssh
-    module Keystore
-      # Your code goes here...
-    end
-  end
-end
--- lib/net/ssh/keystore.rb
+++ lib/net/ssh/keystore.rb
@@ -1,6 +1,7 @@
 module Net
   module Ssh
     module Keystore
+
       VERSION = "0.0.1"
     end
   end
